aac01de1e367762f65573c4d068b561321dc7f8e
Abraham G Musa <44205190+abraham-musa@users.noreply.github.com>
2021-03-17 14:34:48 -0400

    # Purposed solution for continent.py
    Purposed solution for continent.py and added .py file extension to enable code encoding in file.

--- Lists.py
+++ Lists.py
@@ -36,3 +36,26 @@ continents = [
 for continent in continents:
     print ('*' , continent)
 
+# Purposed solution for continent.py
+
+continents = [
+    'Asia',
+    'South America',
+    'North America',
+    'Africa',
+    'Europe',
+    'Antarctica',
+    'Australia',
+]
+
+
+my_list = []
+char = 'A'
+for item in continents:
+  if item[0] in char:
+    my_list.append(item)
+  else:
+    continue
+for continent in my_list:
+  print('* ' + continent)
+
