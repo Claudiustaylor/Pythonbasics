12753728fe7d881fcada7779721475ba57a752f0
Claudiustaylor <taylorclaudius3@gmail.com>
2021-03-19 09:44:45 -0400

    Update to code from missing parameter

--- Team Project.py
+++ Team Project.py
@@ -1,22 +1,44 @@
-import re
-value=[]
-passwords = [i for i in input ("Please enter passwords: ").split(",")]
+import csv
+import random
 
-for password in passwords:
-    if len(password)<8 or len(password)>12:
-        continue
-    else:
-        pass
 
-    if not re.search("[a-z]",password):
-        continue
-    elif not re.search("[A-Z]", password):
-        continue
-    elif not re.search([1-9], password):
-        continue   
-    elif not re.search("[#$@]", password):
-        continue
-    else:
-        pass
-    value.append(password)
-print(",".join(value))
+class PasswordManager:
+
+    def __init__(self):
+        self.csv_path = 'passwords.csv'
+
+        lowercase_letters = list(range(97, 123))
+        uppercase_letters = list(range(65, 91))
+        digits = list(range(48, 58))
+        self.password_chars = lowercase_letters + uppercase_letters + digits
+
+    def main(self):
+        while True:
+            print("Enter 'q' anytime to exit...")
+            if (pass_name := input('Enter the name for the new password you wish to generate: ')) == 'q':
+                break
+
+            new_password = self.generate_new_password()
+            print(f'New password for "{pass_name}" is {new_password}')
+
+            if (response := input('Would you like to save this new password? (y/n): ')) == 'y':
+                self.save_password(pass_name, new_password)
+                print(f'{pass_name} saved successfully.')
+            elif response == 'q':
+                break
+
+    def generate_new_password(self, length=15, special_chars='!$^&*'):
+        return ''.join([self.generate_random_char(special_chars) for _ in range(length)])
+
+    def generate_random_char(self, special_chars=''):
+        return chr(random.choice(self.password_chars + [ord(c) for c in special_chars]))
+
+    def save_password(self, name, password):
+        with open(self.csv_path, 'a', newline='') as file:
+            password_writer = csv.writer(file, delimiter=',')
+            password_writer.writerow([name, password])
+        return True
+
+if __name__ == '__main__':
+    password_manager = PasswordManager()
+    password_manager.main()
